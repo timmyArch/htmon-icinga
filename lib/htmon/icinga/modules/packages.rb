b52fcec7b6c8b45f32b40572774f47cf86ed5b8b
Tim Foerster <github@mailserver.1n3t.de>
2016-02-22 20:46:54 +0100

    add ability for different update checks, such as gems and system, minor version bump
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- lib/htmon/icinga/modules/packages.rb
+++ lib/htmon/icinga/modules/packages.rb
@@ -3,18 +3,24 @@ module Htmon
   module Icinga
     class Module
       class Packages < Module
+
+        Value ||= Struct.new(:name, :amount)
+
+        callback :handle_value do |value, metric|
+          Value.new(metric.split("::").last, value.to_i)
+        end
         
         callback :on_ok do |value, warn, thresh|
-          if value.to_i < warn.to_i
-            "#{value.to_i} packages are out of date."+
-              "| packages=#{value.to_i};#{warn.to_i}"
+          if value.amount < warn.to_i
+            "#{value.amount} (#{value.name}) packages are out of date."+
+              "| packages=#{value.amount};#{warn.to_i}"
           end
         end
         
         callback :on_warn do |value, warn, thresh|
-          if value.to_i >= warn.to_i
-            "#{value.to_i} packages are out of date, update needed."+
-              "| packages=#{value.to_i};#{warn.to_i}"
+          if value.amount >= warn.to_i
+            "#{value.amount} (#{value.name}) packages are out of date, update needed."+
+              "| packages=#{value.amount};#{warn.to_i}"
           end
         end
 
